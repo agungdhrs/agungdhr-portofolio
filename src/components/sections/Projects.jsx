3c300881dc5e0073f266875cc02b77f98552b02a
agungdhrs <generalagungdhr@gmail.com>
2025-06-14 15:47:10 +0700

    add project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -29,7 +29,6 @@ export const Projects = () => {
                   </span>
                 ))}
               </div>
-
               <div className="flex justify-between items-center">
                 <a
                   href="https://web-production-e2c49.up.railway.app/"
@@ -43,25 +42,36 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
               <h3 className="text-xl font-bold mb-2">Student Graduation Prediction Project Using Machine Learning</h3>
               <p className="text-gray-400 mb-4">
-              Developed a machine learning application to predict student graduation outcomes based on academic and demographic data. The project involved data preprocessing, feature selection, training a Random Forest classifier, and evaluating model performance to support early identification of at-risk students.</p>
+                Developed a machine learning application to predict student graduation outcomes based on academic and demographic data. The project involved data preprocessing, feature selection, training a Random Forest classifier, and evaluating model performance to support early identification of at-risk students.
+              </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "Random Forest"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                {["Python", "Random Forest"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
                     "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
               </div>
-
               <div className="flex justify-between items-center">
                 <a
                   href="https://aplikasikinerjamahasiswa-zorufl5dmfvoeb7fmkcpwp.streamlit.app/"
