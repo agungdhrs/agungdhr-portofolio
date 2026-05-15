61c55237f8299651e3045fd5d9122773893cb4f0
agungdhr <agungdhrs@gmail.com>
2026-05-15 10:11:52 +0700

    chore: update base path in vite.config.js to support Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/agungdhr-portofolio",
+  base: process.env.VERCEL ? "/" : "/agungdhr-portofolio",
 });
